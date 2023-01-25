883b4bee6cf28dceb00f389776e95f7536490edd
MK <simon365@naver.com>
2023-01-25 23:25:10 +0900

    Update move func
    +Lerp
    +MoveTowards

--- Unity_Skills/Assets/1. Obj_Move/Scripts/Move.cs
+++ Unity_Skills/Assets/1. Obj_Move/Scripts/Move.cs
@@ -12,8 +12,10 @@ public class Move : MonoBehaviour
     {
         move_Vector,
         move_Translate,
+        move_Towards,
         move_Addforce,
-        move_Velocity
+        move_Velocity,
+        move_Lerp
     }
 
     private Rigidbody rigid;
@@ -34,14 +36,21 @@ public class Move : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        // with manipulating obj's transformation, no physical calc
+        // equal velocity
         if (myState == moveState.move_Vector)
             transform.position += new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
         else if (myState == moveState.move_Translate)
             transform.Translate(new Vector3(h, 0, v) * moveSpeed * Time.deltaTime);
-        else if (myState == moveState.move_Addforce)
+        else if (myState == moveState.move_Towards)     // set destination
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(h, 0, v), moveSpeed * Time.deltaTime);
+        // with using rigidboy, physical calc
+        else if (myState == moveState.move_Addforce)    // add physical force
             rigid.AddForce(new Vector3(h, 0, v) * moveSpeed);
-        else
+        else if (myState == moveState.move_Velocity)   // modifying obj speed
             rigid.velocity = new Vector3(h, 0, v) * moveSpeed;
+        else
+            transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(h, 0, v), moveSpeed * Time.deltaTime);
     }
 
     private void jump()
