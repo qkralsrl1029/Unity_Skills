d94964155191efb5a0fea4ce15e3186d0b0b7442
MK <simon365@naver.com>
2023-01-28 23:35:21 +0900

    TPS Camera movement

--- Unity_Skills/Assets/2. Camera_Move/Scripts/Camera_MoveMent.cs
+++ Unity_Skills/Assets/2. Camera_Move/Scripts/Camera_MoveMent.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Camera_MoveMent : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] float moveSpeed;
-    [SerializeField] float rotateSpeed;
+    #region 목표
+    /*
+    카메라를 메인 캐릭터의 자식으로 설정하지 않고도, 
+    3인칭 TPS 시점으로 캐릭터를 따라서 자연스러운 회전을 구현하도록 함
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    이를 위해서 Container 객체에 캐릭터와 카메라를 각각 자식으로 두어 둘의 이동을 함께 관리하면서  회전을 분리
+    카메라에는 다시 Holder라는 부모 객체를 만들어 초기 3인칭으로 배치해두었던 구도가 유지되도록 함 
+    */
+    #endregion
 
-    // Update is called once per frame
+    [SerializeField]  private Transform player;
+    [SerializeField] private Transform holder;
+ 
     void Update()
     {
-        
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = holder.rotation.eulerAngles;
+
+        float lockedX = camAngle.x - mouseDelta.y;  // 수직 방향 카메라 움직임 제한
+        if (lockedX < 180)
+            lockedX = Mathf.Clamp(lockedX, -1f, 75f);   //카메라가 수평면 아래로 내려가지 않는 문제를 해결하기 위해 -1로 설정
+        else
+            lockedX = Mathf.Clamp(lockedX, 335f, 361f);
+
+        holder.rotation = Quaternion.Euler(lockedX, camAngle.y + mouseDelta.x, camAngle.z);
     }
 }
